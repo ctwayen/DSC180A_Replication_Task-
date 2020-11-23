5d5f3c2949adf9e004d45360222fcfb198213ded
ctwayen <x5zhan@ucsd.edu>
2020-11-23 07:48:42 -0800

    adding test

--- run.py
+++ run.py
@@ -52,17 +52,25 @@ def main():
                         help='number of epochs to train (default: 50)')
     parser.add_argument('--lr', type=float, default=1e-3,
                         help='learning rate (default: 1e-3)')
-    
+    parser.add_argument('--test', action = 'store_true', help='running test')
     args = parser.parse_args()
-    cora = cora_loader(args.cora_path + '/cora.content', args.cora_path + '/cora.cites', args.image_path)
-    X, y, A = cora.get_train()
-    if args.model == 'graph':
-        model = GNN(hidden_neurons=args.hidden_neurons, learning_rate=args.lr, epoch=args.epochs, device=args.device)
+    if args.test:
+        with open('test/testdata/test.json') as f:
+             data = json.load(f)
+        X, y, A = np.array(data['X']), np.array(data['y']), np.array(data['A'])
+        model = GNN()
         model.fit(X, y, A)
-        hist = model.train()
-    if args.model == 'LPA_GCN':
-        model = LPA_GCN(A, X, y, device = args.device, len_walk = args.len_walk)
-        hist = model.train_model(epochs = args.epochs, lr=args.lr)
+        hist = model.train_epoch(F=2, class_number=2)
+    else:
+        cora = cora_loader(args.cora_path + '/cora.content', args.cora_path + '/cora.cites', args.image_path)
+        X, y, A = cora.get_train()
+        if args.model == 'graph':
+            model = GNN(hidden_neurons=args.hidden_neurons, learning_rate=args.lr, epoch=args.epochs, device=args.device)
+            model.fit(X, y, A)
+            hist = model.train_epoch()
+        if args.model == 'LPA_GCN':
+            model = LPA_GCN(A, X, y, device = args.device, len_walk = args.len_walk)
+            hist = model.train_model(epochs = args.epochs, lr=args.lr)
     with open(args.output_path, 'w') as f:
             json.dump(hist, f)
         
