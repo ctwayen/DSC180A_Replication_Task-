5f10a3ebadaf4c6f77bbaabdef241f44085ac865
ctwayen <x5zhan@ucsd.edu>
2020-12-09 10:11:21 -0800

    add arxiv data

--- src/arxiv_loader.py
+++ src/arxiv_loader.py
@@ -5,7 +5,7 @@ import numpy as np
 import scipy
 
 class arxiv_loader():
-    def __init__(self, seed=40, size=0.05, edegs_path="data/edges.csv", labels_path="data/labels.csv", f1_path='data/features1.npz', f2_path='data/features2.npz'):
+    def __init__(self, seed=40, size=0.05, edegs_path="data/edges.csv", labels_path="data/labels.csv", f1_path='data/f_1.npz', f2_path='data/f_2.npz', f3_path='data/f_3.npz', f4_path='data/f_4.npz', f5_path='data/f_5.npz', f6_path='data/f_6.npz'):
         np.random.seed(seed)
         ind = np.random.choice(169343, round(169343*size), replace=False)
         start = pd.read_csv(edegs_path)['start']
@@ -22,6 +22,10 @@ class arxiv_loader():
         self.A = np.asarray(A)
         f_1 = scipy.sparse.load_npz(f1_path).todense()
         f_2 = scipy.sparse.load_npz(f2_path).todense()
-        self.X = np.concatenate([f_1, f_2], axis=0)[ind]
+        f_3 = scipy.sparse.load_npz(f3_path).todense()
+        f_4 = scipy.sparse.load_npz(f4_path).todense()
+        f_5 = scipy.sparse.load_npz(f5_path).todense()
+        f_6 = scipy.sparse.load_npz(f6_path).todense()
+        self.X = np.concatenate([f_1, f_2, f_3, f_4, f_5, f_6], axis=0)[ind]
     def get_train(self):
         return self.A, self.X, self.y
